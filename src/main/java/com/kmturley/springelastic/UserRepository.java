5b1007da4ef164563b165abc374ad17c0e149917
Kim T <kimturley@gmail.com>
2020-06-03 20:16:36 -0700

    Added MySQL database

---
+++ src/main/java/com/kmturley/springelastic/UserRepository.java
@@ -0,0 +1,12 @@
+package com.kmturley.springelastic;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.kmturley.springelastic.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
